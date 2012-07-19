4122849d930814c4220c4240bcbf32e3ddfa2d37
Shane Pfaffly <spfaffly@gmail.com>
2012-07-19 15:46:52 -0500

    Page scroll!

--- js/main.js
+++ js/main.js
@@ -1,42 +1,10 @@
 /* Author: River to Well */
 
 $(document).ready(function(){
-	// Scrollto function
-	$('#mainnav a').on('click', function(event){
-      event.preventDefault();	// So it doesn't drop
-      var section = $(this).attr('href');
-      if($(section).length > 0){
-         $('html, body').animate({
-            scrollTop: $(section).offset().top - 120
-         }, 1000);
-	 	} else {
-	 		$('html, body').animate({scrollTop: 0}, 1000);
-	 	}
-	 	return false;
-   });
-
-	// grab the initial top offset of the navigation 
-  var sticky_navigation_offset_top = $('#fixednav').offset().top;
-   
-  // our function that decides weather the navigation bar should have "fixed" css position or not.
-  var sticky_navigation = function(){
-      var scroll_top = $(window).scrollTop(); // our current vertical position from the top
-       
-      // if we've scrolled more than the navigation, change its position to fixed to stick to top,
-      // otherwise change it back to relative
-      if (scroll_top > sticky_navigation_offset_top) { 
-          $('#fixednav').addClass('fixed');
-      } else {
-          $('#fixednav').removeClass('fixed'); 
-      }   
-  };
-   
-  // run our function on load
-  sticky_navigation();
-   
-  // and run it again every time you scroll
-  $(window).scroll(function() {
-       sticky_navigation();
+  // initiate page scroller plugin
+  $('body').pageScroller({
+    navigation: '#mainnav',
+    scrollOffset: -130
   });
   
   /* Competition Carousel */
