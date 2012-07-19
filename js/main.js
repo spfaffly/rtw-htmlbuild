8fbdfa009fc2256e8b78f4d434afbeee90e0640b
Shane Pfaffly <spfaffly@gmail.com>
2012-07-19 11:34:58 -0500

    External CSS and sticky nav.

--- js/main.js
+++ js/main.js
@@ -16,28 +16,28 @@ $(document).ready(function(){
    });
 
 	// grab the initial top offset of the navigation 
-    // var sticky_navigation_offset_top = $('#fixednav').offset().top;
-     
-    // // our function that decides weather the navigation bar should have "fixed" css position or not.
-    // var sticky_navigation = function(){
-    //     var scroll_top = $(window).scrollTop(); // our current vertical position from the top
-         
-    //     // if we've scrolled more than the navigation, change its position to fixed to stick to top,
-    //     // otherwise change it back to relative
-    //     if (scroll_top > sticky_navigation_offset_top) { 
-    //         $('#fixednav').css({ 'position': 'fixed', 'top':0, 'left':0, 'z-index':999, 'width':'100%' });
-    //     } else {
-    //         $('#fixednav').css({ 'position': 'relative' }); 
-    //     }   
-    // };
-     
-    // // run our function on load
-    // sticky_navigation();
-     
-    // // and run it again every time you scroll
-    // $(window).scroll(function() {
-    //      sticky_navigation();
-    // });
+  var sticky_navigation_offset_top = $('#fixednav').offset().top;
+   
+  // our function that decides weather the navigation bar should have "fixed" css position or not.
+  var sticky_navigation = function(){
+      var scroll_top = $(window).scrollTop(); // our current vertical position from the top
+       
+      // if we've scrolled more than the navigation, change its position to fixed to stick to top,
+      // otherwise change it back to relative
+      if (scroll_top > sticky_navigation_offset_top) { 
+          $('#fixednav').addClass('fixed');
+      } else {
+          $('#fixednav').removeClass('fixed'); 
+      }   
+  };
+   
+  // run our function on load
+  sticky_navigation();
+   
+  // and run it again every time you scroll
+  $(window).scroll(function() {
+       sticky_navigation();
+  });
   
   /* Competition Carousel */
   $("#competition .carousel ul").carouFredSel({
